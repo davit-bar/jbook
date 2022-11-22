f32d688373a33f3e9c79dcd2130509a98019f8b6
Davit Barblishvili <davit.barblishvili@datadoghq.com>
2022-11-22 00:23:39 -0500

    adding redux template

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import { ActionType } from "./action-types";
+import { code } from "@uiw/react-md-editor/lib/cjs/commands";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
